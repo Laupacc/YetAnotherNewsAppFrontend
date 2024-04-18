112be4f52cbb9370a277224cf0798d04b60a6caa
Your name <Your email>
2024-04-18 14:21:32 +0200

    first

---
+++ components/Article.js
@@ -0,0 +1,59 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { addBookmark, removeBookmark } from '../reducers/bookmarks';
+import { hideArticle, unhideArticle } from '../reducers/hiddenArticles';
+import Image from 'next/image';
+import styles from '../styles/Article.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBookmark, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
+
+function Article(props) {
+	const dispatch = useDispatch();
+	const user = useSelector((state) => state.user.value);
+
+// Bookmarks
+	const handleBookmarkClick = () => {
+		if (!user.token) {
+			return;
+		}
+
+		fetch(`http://localhost:3000/users/canBookmark/${user.token}`)
+			.then(response => response.json())
+			.then(data => {
+				if (data.result && data.canBookmark) {
+					if (props.isBookmarked) {
+						dispatch(removeBookmark(props));
+					} else {
+						dispatch(addBookmark(props));
+					}
+				}
+			});
+	}
+
+	let iconStyle = {};
+	if (props.isBookmarked) {
+		iconStyle = { 'color': '#E9BE59' };
+	}
+
+// Hidden articles
+
+	const handleEyeClick = () => {
+			dispatch(hideArticle(props));
+		}
+
+
+	return (
+		<div className={styles.articles}>
+			<div className={styles.articleHeader}>
+				<h3>{props.title}</h3>
+				<FontAwesomeIcon onClick={() => handleBookmarkClick()} icon={faBookmark} style={iconStyle} className={styles.bookmarkIcon} />
+				<FontAwesomeIcon onClick={() => handleEyeClick()} icon={faEyeSlash} className={styles.hiddenIcon} />
+			</div>
+			<h4 style={{ textAlign: "right" }}>- {props.author}</h4>
+			<div className={styles.divider}></div>
+			<Image src={props.urlToImage} alt={props.title} width={600} height={314} />
+			<p>{props.description}</p>
+		</div>
+	);
+}
+
+export default Article;
